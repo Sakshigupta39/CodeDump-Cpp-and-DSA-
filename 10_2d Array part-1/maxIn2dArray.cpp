6bd70eb1c335f8ef3a2cdc4926017195f133ba07
SakshiGupta <sakshigupta3904@gmail.com>
2025-12-14 20:13:22 +0530

    Added  2D array files

---
+++ 10_2d Array part-1/maxIn2dArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter no. of rows : ";
+    cin>>m;
+    int n;
+    cout<<"Enter no. of columns : ";
+    cin>>n;
+    int arr[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    //max
+    int max = INT_MIN;
+     for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(max<arr[i][j]) max=arr[i][j];
+        
+        }
+    }
+    cout<<"Maximum element = "<<max;
+    
+}
---
+++ 11_2d Array Part-2/assignmentquestion.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter no. of row matrix : ";
+    cin>>m;
+    int arr[m][m];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    int minr=0;
+    int minc=0;
+    int maxr=m-1;
+    int maxc=m-1;
+
+    //print spiral
+    while(minr<=maxr && minc<=maxc){
+        //right
+        for(int j=minc;j<=maxc;j++){
+            cout<<arr[minr][j]<<" ";
+        }
+        minr++;
+        if(minr>maxr || minc>maxc) break;
+        //down
+        for(int i=minr;i<=maxr;i++){
+            cout<<arr[i][maxc]<<" ";
+        }
+        maxc--;
+        if(minr>maxr || minc>maxc) break;
+        //left
+        for(int j=maxc;j>=minc;j--){
+            cout<<arr[maxr][j]<<" ";
+        }
+        maxr--;
+        if(minr>maxr || minc>maxc) break;
+        //top
+        for(int i=maxr;i>=minr;i--){
+            cout<<arr[i][minc]<<" ";
+        }
+        minc++;    
+    }
+
+}
